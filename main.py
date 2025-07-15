7ba00645197736d626291ed675434d94756bd3ef
dev-ggomes <gomesguilherme.pt@gmail.com>
2025-07-15 11:54:07 +0100

    test pt.2

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 from lexer import Lexer
+from parser import Parser
 
 code = '''
-let nome = "fluxa"
-let x = 10
-print(nome + x)
+let nome = "Fluxa"
+let idade = 18
+print(nome + idade)
 '''
 
+# Etapa 1: Lexer
 lexer = Lexer(code)
 tokens = lexer.tokenize()
 
-for token in tokens:
-    print(token)
+# Etapa 2: Parser
+parser = Parser(tokens)
+ast = parser.parse()
+
+# Etapa 3: Mostrar AST
+for node in ast:
+    print(node)

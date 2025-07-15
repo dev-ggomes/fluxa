5f9dd3a386839360d9607e2cb804dc038bfbe456
dev-ggomes <gomesguilherme.pt@gmail.com>
2025-07-15 11:56:23 +0100

    nodes.py updated

--- language/nodes.py
+++ language/nodes.py
@@ -5,13 +5,22 @@ class NumberNode(Node):
     def __init__(self, value):
         self.value = value
 
+    def __repr__(self):
+        return f'NumberNode({self.value})'
+    
 class StringNode(Node): 
     def __init__(self, value):
         self.value = value
 
+    def __repr__(self):
+        return f'StringNode({self.value})'
+
 class IdentifierNode(Node):
     def __init__(self, value):
         self.value = value
+
+    def __repr__(self):
+        return f'IdentifierNode({self.name})'
 
 class BinaryOpNode(Node):
     def __init__(self, left, op, right):
@@ -19,11 +28,20 @@ class BinaryOpNode(Node):
         self.op = op
         self.right = right
 
+    def __repr__(self):
+        return f'BinaryOpNode({self.left}, {self.op}, {self.right})'
+
 class VarAssignNode(Node):
     def __init__(self, name, value):
         self.name = name
         self.value = value
 
+    def __repr__(self):
+        return f'VarAssignNode({self.name}, {self.value})'
+
 class PrintNode(Node): 
     def __init__(self, expression):
         self.expression = expression
+
+    def __repr__(self):
+        return f'PrintNode({self.expression})'
